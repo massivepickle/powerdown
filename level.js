b1cb0bbcc194975a376dc8eefd5e7cc82d2aa471
proqbr <58514716+proqbr@users.noreply.github.com>
2020-06-16 16:13:00 +0530

    Update level.js

--- level.js
+++ level.js
@@ -161,7 +161,7 @@ class Level{
                             }
                         }
                     }
-                    if(20 > rbt[0].over + rbt[1].over > 0 /*&& rbt[0].over + rbt[1].over < 20*/){
+                    if(20 > rbt[0].over + rbt[1].over && rbt[0].over + rbt[1].over > 5){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -190,7 +190,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 2){
@@ -323,7 +323,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -352,7 +352,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 3){
@@ -502,7 +502,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -531,7 +531,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 4){
@@ -695,7 +695,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -724,7 +724,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 5){
@@ -891,7 +891,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -920,7 +920,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 6){
@@ -1092,7 +1092,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -1121,7 +1121,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 7){
@@ -1298,7 +1298,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -1327,7 +1327,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 8){
@@ -1509,7 +1509,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -1537,7 +1537,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10)
                     }
                 }
             }else if(lvl === 9){
@@ -1724,7 +1724,7 @@ class Level{
                             }
                         }
                     }
-                    if(rbt[0].over + rbt[1].over > 0 && rbt[0].over + rbt[1].over < 20){
+                    if(rbt[0].over + rbt[1].over > 5 && rbt[0].over + rbt[1].over < 20){
                         flag = 0;
                     }
                     if(rbt[0].over + rbt[1].over < 2){
@@ -1753,7 +1753,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10);
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10);
                     }
                 }
             }else if(lvl === 10){
@@ -2011,7 +2011,10 @@ class Level{
                             text("APPEAR IN: " + roundoff(appearin-10+endbreaks,1), camera.position.x+2.5, camera.position.y - windowHeight/(camera.zoom * 2) + 18);
                         }
                     }
-                    if(endbreaks >= 5){
+                    if(endbreaks === 5){
+                        flag = 0;
+                        endbreaks += 1;
+                    }else if(endbreaks > 5){
                         won = true;
                         bossMusic.setVolume(0,0.25);
                         bossMusic2.setVolume(0,0.25);
@@ -2110,7 +2113,7 @@ class Level{
                         stroke(0,0,0,180);
                         strokeWeight(1);
                         textStyle(BOLDITALIC);
-                        text("\nTime: "+await roundoff(levelTime,3)+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx,texty+10);
+                        text("\nTime: "+roundoff(levelTime,3)+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx,texty+10);
                     }
                     if(rbt[0].dashin >= -0.5 && !go){
                         textAlign(CENTER);
@@ -2172,7 +2175,7 @@ class Level{
                 stroke(0,0,0,180);
                 strokeWeight(1);
                 textStyle(BOLDITALIC);
-                text("\nTime: "+levelTime+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx1,texty1+10);
+                text("\nTime: "+levelTime+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx1,texty1+10);
                 if(mouseIsPressed || flag === 1){
                     change = true;
                     changeto = "menu";
@@ -2218,7 +2221,7 @@ class Level{
                 stroke(0,0,0,180);
                 strokeWeight(1);
                 textStyle(BOLDITALIC);
-                text("\nTime: "+levelTime+"s\nTotal Time: "+await roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+await roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+await roundoff(totalTime/totalWallbreaks,3),textx1,texty1+10);
+                text("\nTime: "+levelTime+"s\nTotal Time: "+roundoff(totalTime,3)+"s\nWall Breaks: "+wallbreaks+"\nTotal Wall Breaks: "+totalWallbreaks+"\nTime/Wall Breaks: "+roundoff(ltwbRatio,3)+"\nTotal Time/Total Wall Breaks: "+roundoff(totalTime/totalWallbreaks,3),textx1,texty1+10);
                 mainMusic.setVolume(0,0.9);
                 bossMusic.setVolume(0,0.9);
                 bossMusic2.setVolume(0,0.9);
